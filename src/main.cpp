15d4329e440149f1597ff6c42f36b88f1595ecf1
Ruslana478 <ruslanapopovych2004@gmail.com>
2024-02-14 15:53:36 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "FileSearcherLibrary.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3){
+        std::cout << "Invalid usage: ./file_searcher FILE_TO_SEARCH.EXTENSION ROOT_DIRECTORY" << std::endl;
+        return 0;
+    }
+    FileSearcher::search_for_the_file_in_directory(argv[1], argv[2]);
+    return 0;
+}
